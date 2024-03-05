1e79751dcbe9dc5412aef8e6068961af81844426
root <root@ip-172-31-47-155.ap-south-1.compute.internal>
2024-03-05 06:33:54 +0000

    luv u nigu

--- file.java
+++ file.java
@@ -21,17 +21,6 @@ public class SpiralPatternExample2
 	////calculates and prints the values for pattern  
 	//System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");  
 	//}  
-	//System.out.println();  
-	//}  
-	//}  
-	////driver code  
-	//public static void main(String args[])  
-	//{  
-	//Scanner sc = new Scanner(System.in);  
-	//System.out.print("Enter the value of n: ");  
-	//int n = sc.nextInt();  
-	//System.out.println();  
-	////function calling  
-	//printPattern(n);  
-	//}  
-	//} 
+	//System.out.println(); 
+	//nigu you are my love
+	//
